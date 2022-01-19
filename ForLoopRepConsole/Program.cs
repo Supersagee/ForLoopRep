1ab5e4b8cc89291be84f9a844bf69137171353df
Supersagee <williamdsage@gmail.com>
2022-01-18 21:43:51 -0600

    first commit

---
+++ ForLoopRepConsole/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace ForLoopRepConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }    
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }*/
+        }
+    }
+}
